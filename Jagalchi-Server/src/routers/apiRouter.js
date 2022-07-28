dea4432d45a3ee2e140b9e764d33d2335f51f750
bandall <jsm5315@naver.com>
2022-07-28 21:38:33 +0900

    babel error fix

--- Jagalchi-Server/src/routers/apiRouter.js
+++ Jagalchi-Server/src/routers/apiRouter.js
@@ -1,11 +1,10 @@
 import express from "express";
 import { loginOnlyMiddleWare } from "../middlewares";
 import { getUserInfo } from "../controllers/apiController";
-import { getPost } from "../controllers/postController";
-import { postRecommand } from "../../../jagalchi-front/src/components/functions/postAPI";
+import { getPost, recommandPost } from "../controllers/postController";
 const apiRouter = express.Router();
 
 apiRouter.route("/userinfo").get(getUserInfo);
 apiRouter.route("/post/:postID([0-9a-f]{24})").get(getPost);
-apiRouter.route("post/:postID([0-9a-f]{24})/recommand").all(loginOnlyMiddleWare).post(postRecommand);
+apiRouter.route("/post/:postID([0-9a-f]{24})/recommand").all(loginOnlyMiddleWare).post(recommandPost);
 export default apiRouter;
