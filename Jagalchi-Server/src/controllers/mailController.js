e785bf250f0bac8e70f862fc192d62bb722f3666
bandall <jsm5315@naver.com>
2022-09-11 22:43:39 +0900

    send email html

--- Jagalchi-Server/src/controllers/mailController.js
+++ Jagalchi-Server/src/controllers/mailController.js
@@ -15,25 +15,38 @@ const smtpTransport = nodemailer.createTransport({
     }
 });
 
-const getSecureRandom = () => {
-    const random = crypto.randomBytes(4).toString("hex").toUpperCase();
-    return random;
-}
-
 export const sendAuthEmail = async (req, res) => {
     const { email } = req.query;
-    console.log(smtpTransport);
+    const random = crypto.randomBytes(4).toString("hex").toUpperCase();
+    const html = `
+        <div>
+            <div style="display: flex;">
+                <img src="http://localhost:4000/static/favicon.ico" style="width: 50px; height: 50px; margin-top: 21px; margin-bottom: 21px;">
+                <h1>자갈치 시장</h1>
+            </div>
+
+            <div>
+                <h2>안녕하세요 {}님.</h2>
+                <h2>{}님의 인증번호는 ${random}입니다.</h2>
+            </div>
+        </div>
+    `;
+
+    
     const mailOptions = {
         from: process.env.SMTP_USER,
         to: email,
         subject: "[자갈치 시장] 회원님의 인증번호가 발급되었습니다.",
-        text: "hello" + getSecureRandom(),
-        html: ""
+        text: "",
+        html: html
     }
+
     smtpTransport.sendMail(mailOptions, (err, response) => {
-        console.log(err);
-        console.log(response);
+        if(err) {
+            console.log(err);
+        }
         smtpTransport.close();
     })
-    return res.send("a");
+
+    return res.sendStatus(200);
 }
